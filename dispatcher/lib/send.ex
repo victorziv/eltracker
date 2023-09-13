bc883dff55608a2805fefdd70986f97be7c4efb7
Victor Ziv <vziv@infinidat.com>
2023-09-13 20:53:51 +0300

    Start dispatcher and executor

---
+++ dispatcher/lib/send.ex
@@ -0,0 +1,10 @@
+defmodule Send do
+  def go do
+    {:ok, connection} = AMQP.Connection.open
+    {:ok, channel} = AMQP.Channel.open(connection)
+    AMQP.Queue.declare(channel, "hello")
+    AMQP.Basic.publish(channel, "", "hello", "Hello World!")
+    IO.puts " [x] Sent 'Hello World!'"
+    AMQP.Connection.close(connection)
+  end
+end
--- dispatcher/mix.exs
+++ dispatcher/mix.exs
@@ -14,7 +14,7 @@ defmodule Dispatcher.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :amqp]
     ]
   end
 
@@ -23,6 +23,7 @@ defmodule Dispatcher.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:amqp, "~> 3.3"}, 
     ]
   end
 end
