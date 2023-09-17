bc76f60044b43da6f56d4dbd57ba8027ce029270
Victor Ziv <vziv@infinidat.com>
2023-09-17 18:54:52 +0300

    Fix a bug in Dispatcher.start

--- dispatcher/lib/dispatcher.ex
+++ dispatcher/lib/dispatcher.ex
@@ -1,10 +1,22 @@
 defmodule Dispatcher do
 
   def start do
-    spawn(fn _ ->
+    spawn(fn ->
       connection = :rand.uniform(1000)
       loop(connection)
     end)
+  end
+
+   def run_async(server_pid, query_def) do
+    send(server_pid, {:run_query, self(), query_def})
+  end
+
+  def get_result do
+    receive do
+      {:query_result, result} -> result
+    after
+      5000 -> {:error, :timeout}
+    end
   end
 
   defp loop(connection) do
